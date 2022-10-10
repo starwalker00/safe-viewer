093e62258c66ebbdee8cf33f6d5b261470b87684
starwalker00 <aaronwalkerup@gmail.com>
2022-10-10 21:13:49 +0200

    feat: add search bar

--- pages/components/SafeList.tsx
+++ pages/components/SafeList.tsx
@@ -1,19 +1,30 @@
 import { useSuspense } from "rest-hooks";
 import { RestEndpoint } from "@rest-hooks/rest";
+import { useState } from "react";
 
 const getSafes = new RestEndpoint({
     urlPrefix: 'https://safe-transaction.gnosis.io',
-    path: '/api/v1/owners/:address/safes/',
+    path: '/api/v1/owners/:ownerAddress/safes/',
 });
 
 export function SafeList() {
-    const address: string = "0x2E21f5d32841cf8C7da805185A041400bF15f21A";//stani.eth
-    const { safes } = useSuspense(getSafes, { address });
+    const stanieth: string = "0x2E21f5d32841cf8C7da805185A041400bF15f21A";//stani.eth
+    const [ownerAddress, setOwnerAddress] = useState(stanieth);
+    const { safes } = useSuspense(getSafes, { ownerAddress });
+    console.log(ownerAddress)
     console.log(safes)
     return (
-        safes.map((safeAddress: any) => {
-            return (<Safe address={safeAddress} />)
-        })
+        <>
+            <SearchBar
+                searchQuery={ownerAddress}
+                setSearchQuery={setOwnerAddress}
+            />
+            {
+                safes.map((safeAddress: any) => {
+                    return (<Safe address={safeAddress} />)
+                })
+            }
+        </>
     )
 }
 
@@ -21,10 +32,31 @@ interface SafeProps {
     address: string;
 }
 function Safe({ address }: SafeProps) {
-    console.log(address)
+    //console.log(address)
     return (
         <div>
             <p>{address}</p>
         </div>
+    )
+}
+
+interface SearchBarProps {
+    searchQuery: any;
+    setSearchQuery: any
+}
+function SearchBar({ searchQuery, setSearchQuery }: SearchBarProps) {
+    const [value, setValue] = useState(searchQuery);
+    return (
+        <form onSubmit={e => e.preventDefault()}>
+            <input
+                value={value}
+                onChange={(event) => setValue((event.target as HTMLInputElement).value)}
+                type="text"
+            />
+            <button type="button"
+                onClick={() => setSearchQuery(value)}>
+                Search
+            </button>
+        </form >
     )
 }
